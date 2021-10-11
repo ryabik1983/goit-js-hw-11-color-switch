2e5751ea6973d3660f79012723c4ffeaf5fd7b8d
ryabik1983 <81900360+ryabik1983@users.noreply.github.com>
2021-10-11 16:32:58 +0300

    11
    11

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ const colors = [
   const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
+
+  import BSN from 'bootstrap.native';
+
+  // console.log('Это консоль !');
+  // const PROMT_DELAY = 1000;
+  // const MAX_PROMPT_ATTEMPTS = 3;
+  // let promptCounter = 0;
+  // let hasSubscribed = false;
+  //  setInterval (() => {
+  //    console.log('Подпишитесь на рассылку! -' + Date.now())
+  //   }, PROMT_DELAY);
+
